9b36dd7ed459853b43ec1ea62271176b6f85f899
Michaël Defferrard <michael.defferrard@epfl.ch>
2020-01-21 10:49:39 +0100

    while sometimes convenient, showing all outputs is sometimes painful and non-standard

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,4 +1,5 @@
 c = get_config()
 
 # Run all nodes interactively.
-c.InteractiveShell.ast_node_interactivity = "all"
+# https://stackoverflow.com/questions/36786722/how-to-display-full-output-in-jupyter-not-only-last-result
+# c.InteractiveShell.ast_node_interactivity = "all"
